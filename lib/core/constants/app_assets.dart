1887f59f59574e3e521e8261e66d77bee6b8ff39
TintWaiYanMin <tintwaiyanmin.sg@gmail.com>
2026-05-16 20:01:16 +0800

    Updates codes

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,6 +1,10 @@
 /// Declared asset paths (images, icons, fonts registered in `pubspec.yaml`).
 abstract final class AppAssets {
   static const logo = 'assets/images/logo.png';
+  static const logoFull = 'assets/images/novora-logo.png';
+  static const logoIcon = 'assets/images/novora-logo-icon.png';
+  static const logoSvg = 'assets/images/novora-logo.svg';
+  static const logoIconSvg = 'assets/images/novora-logo-icon.svg';
   static const logoWhite = 'assets/images/logo_white.png';
   static const onboardingBg = 'assets/images/onboarding_bg.png';
   static const appIcon = 'assets/icons/app_icon.png';
